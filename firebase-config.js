d947e8d1e13a8b0a1cb13271d5fc5f63c4570685
s-abe06 <abeshi06@gmail.com>
2026-09-03 08:41:24 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,15 @@
 // 下の { ... } の中に貼り替えてください。
 // ==========================================================
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyD0N-PqqO47criDFqBsW-agxjEQIVcqV_c",
+  authDomain: "spy-c7074.firebaseapp.com",
+  databaseURL: "https://spy-c7074-default-rtdb.firebaseio.com",
+  projectId: "spy-c7074",
+  storageBucket: "spy-c7074.firebasestorage.app",
+  messagingSenderId: "330886035877",
+  appId: "1:330886035877:web:90dca3c966776823d8104c",
+  measurementId: "G-EPYP41519Z"
 };
-
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
